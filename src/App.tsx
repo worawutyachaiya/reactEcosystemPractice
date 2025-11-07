a3ca4c9612e5649f96c9df48d6f7d1a19c4e7322
worawut yachaiya <worawutavc@gmail.com>
2025-11-07 12:43:39 +0700

    addOn

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-// import Query from "./components/Query";
+import Query from "./components/Query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Register from "./components/Register";
+// import Register from "./components/Register";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <Query /> */}
-      <Register/>
+      <Query />
     </QueryClientProvider>
   );
 };
--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -1,25 +1,69 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 
 const Query = () => {
-  const { isPending, error, data, refetch, isFetching } = useQuery({
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const queryClient = useQueryClient();
+
+  // ✅ GET Users
+  const { data, isPending, error, refetch, isFetching } = useQuery({
     queryKey: ["repoData"],
-    queryFn: (): Promise<any> =>
-      fetch("https://jsonplaceholder.typicode.com/users").then(
-        (res): Promise<any> => res.json()
-      ),
+    queryFn: async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      return res.data;
+    },
   });
 
-  // if (isPending) return "Loading...";
+  // ✅ POST User
+  const mutation = useMutation({
+    mutationFn: (newUser: { name: string; email: string }) =>
+      axios.post("https://jsonplaceholder.typicode.com/users", newUser),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:["repoData"]});
+      setEmail("");
+      setName(""); // รีเซ็ต input
+      console.log("New user is",email,name)
+    },
+  });
 
-  if (error) return "An error has occurred: " + error.message;
+  const handleAddUser = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // e.preventDefault();
+    mutation.mutate({ name, email });
+    
+  };
+
+  if (error) return "An error has occurred: " + JSON.stringify(error);
 
   return (
     <div className="bg-black/60 flex items-center justify-center h-dvh flex-col gap-2">
-      <h1 className="text-2xl">รายชื่อผู้ใช้</h1>
+
+      <input type="text" placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border border-white text-white px-4 py-2"
+      />
+
+      <input type="text" placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="border border-white text-white px-4 py-2"
+      />
+
+      <button
+        onClick={handleAddUser}
+        disabled={mutation.isPending}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:cursor-pointer"
+      >
+        {mutation.isPending ? "Sending..." : "Add"}
+      </button>
+
+      <h1 className="text-2xl text-white">รายชื่อผู้ใช้</h1>
 
       {isPending
         ? "Loading..."
-        : data.map((user: any) => (
+        : data?.map((user: any) => (
             <div key={user.id} className="flex gap-2 text-xl text-gray-600">
               <h2 className="text-white">คนที่ {user.id}</h2>
               <h2 className="text-purple-300">{user.name}</h2>
@@ -37,4 +81,5 @@ const Query = () => {
     </div>
   );
 };
+
 export default Query;
